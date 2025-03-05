32ab880b14ec7c09e53e864a44667d706d5b36fa
sudo-paoo <kingpaolofranco60@gmail.com>
2025-03-06 07:42:01 +0800

    gh-pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true, // Needed for static images
+  },
+  basePath: "/math-game", // Ensures correct routing on GitHub Pages
+  assetPrefix: "/math-game/", // Fixes static asset paths
 };
 
 export default nextConfig;
